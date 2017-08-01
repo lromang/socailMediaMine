41a33f6269f8e920a18be14d60e4568e52fe0b1a
lromang <luis.roangarci@gmail.com>
2017-08-01 13:48:48 -0500

    ...

--- dashboard/public/javascripts/dash.js
+++ dashboard/public/javascripts/dash.js
@@ -1,31 +1,42 @@
 
-$('form').submit(function(e){
-    var form = $(this).serializeArray();
-    form[0].value
-    var win = window.open('/place/' + form[0].value + '/' + form[1].value + '/' + form[3].value + '/' + form[4].value + '/query.json');
-    // win.focus();
+$(document).ready(function(){
     mapboxgl.accessToken = 'pk.eyJ1IjoiZ29ibXgxNSIsImEiOiJjaWZpaTl2eGtibDBjcnNtN3NqdW1wN25xIn0.yxAJmrmgXO_IaXuI1lckYA';
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v9',
-        center: [form[1].value, form.value[0]],
+        center: [-99.177081, 19.401782],
         zoom: 12
     });
+})
 
-    map.addSource('places', {
-        'type':'geojson',
-        'data': 'localhost:3000/place/' + form[0].value + '/' + form[1].value + '/' + form[3].value + '/' + form[4].value + '/query.json'
-    })
 
-    map.on('load', function(){
-        map.addlayer({
-            'id': 'places',
-            'type': 'point',
-            'source': 'places'
+$('form').submit(function(e){
+    var form = $(this).serializeArray();
+    form[0].value
+    // var win = window.open('/place/' + form[0].value + '/' + form[1].value + '/' + form[3].value + '/' + form[4].value + '/query.json');
+    // win.focus();
+    $.get('/place/' + form[0].value + '/' + form[1].value + '/' + form[3].value + '/' + form[4].value + '/query.json').done(function (data) {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZ29ibXgxNSIsImEiOiJjaWZpaTl2eGtibDBjcnNtN3NqdW1wN25xIn0.yxAJmrmgXO_IaXuI1lckYA';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/light-v9',
+            center: [form[1].value, form.value[0]],
+            zoom: 12
+        });
+
+        map.addSource('places', {
+            'type':'geojson',
+            'data': data
         })
-    })
-    /*$.get('/place/' + form[0].value + '/' + form[1].value + '/' + form[3].value + '/' + form[4].value + '/query.json').done(function (data) {
 
-    });*/
+        map.on('load', function(){
+            map.addlayer({
+                'id': 'places',
+                'type': 'point',
+                'source': 'places'
+            })
+        })
+
+    });
     // e.preventDefault();
 })
