2108c5986caa76a476680ce303090465ca413898
lromang <luis.roangarci@gmail.com>
2017-08-14 13:38:55 -0500

    ...

--- clustering/code/code.R
+++ clustering/code/code.R
@@ -55,7 +55,7 @@ toText <- function(messages){
         tolower()                      %>%
         removePunctuation()            %>%
         str_replace_all("\t", "")      %>%
-        ## iconv("UTF-8", "ASCII", "")    %>%
+        iconv("UTF-8", "UTF-8", "")    %>%
         removeWords(stopwords("spanish"))
     messages
 }
@@ -69,4 +69,34 @@ data <- read.csv('../data/bas.csv',
 ## ----------------------------------------
 ## Processing and cleaning variables
 ## ----------------------------------------
-contents <- toText(data$Contenido)
+clean_data <- data.frame(
+    'contents'  = toText(data$Contenido),
+    'date'      = strptime(data$Fecha.Creación,
+                         format = '%m/%d/%Y %H:%M',
+                         tz = 'CT'),
+    'tags'      = toText(data$Tags),
+    'cards'     = data$todas.las.tarjetas,
+    'street'    = data$Y,
+    'zip.code'  = data$Código.Postal,
+    'inBetween' = data$Entre,
+    'cash'      = data$Efectivo,
+    'nei'       = data$Colonia,
+    'corner'    = data$Esquina,
+    'type'      = toText(data$Generos),
+    'lat'       = data$Latitud,
+    'lon'       = data$Longitud,
+    'minPrice'  = data$Precio.inferior,
+    'maxPrice'  = data$Precio.Superior,
+    'mCard'     = data$MasterCard,
+    'amex'      = data$Amex,
+    'visa'      = data$Visa,
+    'numExt'    = data$numExt,
+    'numInt'    = data$numInt,
+    'schedule'  = data$Horario,
+    'services'  = toText(data$Sevicios),
+    'state'     = data$Estado,
+    'del'       = data$Delegación,
+    'street'    = data$Calle,
+    'tel'       = data$Telefono.1,
+    'web'       = data$Sitio.Web,
+)
